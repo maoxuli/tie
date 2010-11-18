ecf0f178b811878643f81df5d5a1ecf0ded78c9e
Maoxu Li <lim@gwmail.gwu.edu>
2010-11-18 13:55:41 -0500

    Tie empty application.

---
+++ tie/src/com/limlabs/tie/Tie.java
@@ -0,0 +1,13 @@
+package com.limlabs.tie;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Tie extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
